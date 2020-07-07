606f641ff524c6ee411f30fbb791cf3d52997c3b
esplaiStPere <67979455+esplaiStPere@users.noreply.github.com>
2020-07-07 22:13:57 +0200

    Add files via upload

---
+++ precache-manifest.6045868a13f277e551cd7c577f726ad3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d332b3daa94f8f0fb33161c150c6ba2b",
+    "url": "/bustia-esplai/index.html"
+  },
+  {
+    "revision": "4cd3948a863dd1823b86",
+    "url": "/bustia-esplai/static/css/main.7f79f2db.chunk.css"
+  },
+  {
+    "revision": "c112b7acd492c95a803c",
+    "url": "/bustia-esplai/static/js/2.b88a3aea.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/bustia-esplai/static/js/2.b88a3aea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cd3948a863dd1823b86",
+    "url": "/bustia-esplai/static/js/main.b7a97017.chunk.js"
+  },
+  {
+    "revision": "ee82a90a8f4bba4e3a08",
+    "url": "/bustia-esplai/static/js/runtime-main.1b7f066a.js"
+  }
+]);
